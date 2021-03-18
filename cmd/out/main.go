3fc11abee4e218f3c87b8ac10fe31a134ee42eea
Toni Schmidbauer <toni@stderr.at>
2021-03-18 18:21:44 +0100

    update

--- cmd/out/main.go
+++ cmd/out/main.go
@@ -21,17 +21,20 @@ func main() {
 
 func newRootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:   filepath.Base(os.Args[0]),
-		Short: "Tests prerequisites for installing OpenShift with the UPI method",
-		Long:  "",
-		SilenceUsage:     true,
+		Use:          filepath.Base(os.Args[0]),
+		Short:        "Tests prerequisites for installing OpenShift with the UPI method",
+		Long:         "",
+		SilenceUsage: true,
 		// Run: runRootCmd,
 	}
 
 	rootCmd.AddCommand(newTestCommand())
 	rootCmd.PersistentFlags().StringVar(&rootOpts.dir, "dir", ".", "directory containing install-config.yaml")
 
+	os.Chdir()
+
 	return rootCmd
+
 }
 
 // func runRootCmd(cmd *cobra.Command, args []string) {
